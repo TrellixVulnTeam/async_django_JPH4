4d0dbe3a241b1c2d68c3918fcdffa4695441498b
Daniel Callaghan <dan_cal89@hotmail.com>
2022-04-10 19:23:40 +0100

    database

--- playground/settings.py
+++ playground/settings.py
@@ -114,19 +114,19 @@ WSGI_APPLICATION = 'playground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# import dj_database_url
-
 # DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
